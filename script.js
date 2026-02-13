75d0831caaf7bda110d436e536b72f4fd4f41bbe
Jakucs Viktor <jakucs42@gmail.com>
2026-02-13 12:21:30 +0100

    print the name of the last product

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 import { albums } from './data.js';
 
 console.log(albums[0])
+let trackName = [];
 
-for(let i of albums){
-    for(let detail of i.details){
-        console.log(detail.track_id)
+for(let album of albums){
+    for(let detail of album.details){
+        console.log(detail)
+        trackName.unshift(detail)
     }
 }
+console.log(trackName[0].name)
 
 
 // Write your code here
