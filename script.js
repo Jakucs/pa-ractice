f373f64b2654a9e4e127b6a95273c3a730078171
Jakucs Viktor <jakucs42@gmail.com>
2026-02-14 10:33:31 +0100

    there are numbers of available albums

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 import { albums } from './data.js';
 
-console.log(albums[0])
+//console.log(albums[0])
+/* let trackName = [];
 
-for(let i of albums){
-    for(let detail of i.details){
-        console.log(detail.track_id)
+for(let album of albums){
+    //console.log(album)
+for(let album of albums){
     }
 }
 
+for(let number in trackName){
+for(let number in trackName){
+    console.log(number)
+}
+}
+console.log(trackName[3502])
+
+ */
 
 // Write your code here
 
-// 
+/* function getSongCountInFirst(albums){
+    let returnNumber = 0;
+
+    for(let i in albums[0].details){
+        returnNumber++
+    }
+   return returnNumber
+        
+}
+
+console.log(getSongCountInFirst(albums)) */
+
+function getAvailableAmount(){
+    let counter = 0;
+    for(let i of albums){
+        if(i.status=="available"){
+            counter++
+        }
+    }
+
+    return counter
+}
+
+console.log(getAvailableAmount())
