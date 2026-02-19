668463b0c5165bf3544339a3904fef0b3d93cf0f
Jakucs Viktor <jakucs42@gmail.com>
2026-02-19 22:35:33 +0100

    search the longest album

--- script.js
+++ script.js
@@ -537,20 +537,3 @@ function searchLongestAlbum(){
 let longestAlbum = searchLongestAlbum()
 
 
-function addElement(){
-    let root = document.getElementById("root")
-    let div = document.createElement("div")
-    let button = document.createElement("button")
-    button.textContent = "Button"
-    
-    button.addEventListener("click", function(e){
-        let h2 = document.createElement("h2")
-        h2.textContent = longestAlbum.name
-        div.appendChild(h2)
-    })
-
-    div.appendChild(button)
-    root.appendChild(div)
-}
-
-addElement()
